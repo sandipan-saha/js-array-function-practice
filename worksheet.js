b00372515e6dc4c1fcb547a417d5e86d2e277fb1
sandipan-saha <sandipansaha.dev@outlook.com>
2024-02-18 00:45:14 +0530

    Practised 'filter()' function

--- worksheet.js
+++ worksheet.js
@@ -1,32 +1,32 @@
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
+  {
+    name: "Luke Skywalker",
+    height: 172,
+    mass: 77,
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: 202,
+    mass: 136,
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: 150,
+    mass: 49,
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: 188,
+    mass: 84,
+    eye_color: "blue",
+    gender: "male",
+  },
 ];
 
 //***MAP***
@@ -43,9 +43,17 @@ const characters = [
 
 //***FILTER***
 //1. Get characters with mass greater than 100
+const massGreater100 = characters.filter((character) => character.mass > 100);
+console.log("Characters with mass greater than 100: ", massGreater100);
 //2. Get characters with height less than 200
+const heightLesser200 = characters.filter((character) => character.height < 200);
+console.log("Characters with height lesser than 200: ", heightLesser200);
 //3. Get all male characters
+const allMale = characters.filter((character) => character.gender === "male");
+console.log("All male characters: ", allMale);
 //4. Get all female characters
+const allFemale = characters.filter((character) => character.gender === "female");
+console.log("All female characters: ", allFemale);
 
 //***SORT***
 //1. Sort by mass
